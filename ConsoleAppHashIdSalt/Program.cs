6da4988a154a0becf54e0a27f087d0ffe8b777f4
o.keskiner <o.keskiner@TT-DT020>
2022-04-19 16:23:35 +0300

    ok diğer dosyalar atıldı

---
+++ ConsoleAppHashIdSalt/Program.cs
@@ -0,0 +1,7 @@
+﻿using HashidsNet;
+var hashids = new Hashids("this is my salt",11);
+var hash = hashids.Encode(1);
+Console.WriteLine(hash);
+var numbers = hashids.Decode(hash);
+Console.WriteLine(numbers[0]);
+Console.ReadLine();
